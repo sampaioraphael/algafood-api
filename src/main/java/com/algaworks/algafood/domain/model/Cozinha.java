822fd394043c0c3866833a8a799fd4eafea0608c
sampaioraphael <rapha-el@msn.com>
2020-06-08 10:36:49 -0300

    Primeiro commit

---
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -0,0 +1,35 @@
+package com.algaworks.algafood.domain.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonRootName;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@JsonRootName("cozinha")
+@Data //Equals and Hashcode/Getter/Setter/@toString
+@EqualsAndHashCode(onlyExplicitlyIncluded=true)
+@Entity
+public class Cozinha {
+	
+	//@NotNull (groups = Groups.CozinhaId.class)
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable=false)
+	private String nome;
+	
+	@OneToMany(mappedBy="cozinha")
+	private List<Restaurante> restaurantes = new ArrayList<>();
+}
